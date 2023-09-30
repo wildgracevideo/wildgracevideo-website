f964e69401d7c79e6ca64cfa666274280796441c
Emmet Murray <ehmurray8@gmail.com>
2023-09-29 18:48:49 -0600

    Fix error messages in functions.

--- functions/contact-submit/contact-submit.ts
+++ functions/contact-submit/contact-submit.ts
@@ -33,7 +33,7 @@ export const handler: Handler = async (event, context) => {
     console.error('Token body not found.');
     return FORBIDDEN_BODY;
   } else if (!RECAPTCHA_SECRET_KEY) {
-    console.error('Token body not found.');
+    console.error('Recaptcha secret key not found.');
     return FORBIDDEN_BODY;
   } else {
     if (!(await validateRecaptcha(RECAPTCHA_SECRET_KEY, body.token, 'contact_submit'))) {
