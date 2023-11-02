aa23ed853bbac021db54020e1cbdd5b6786c738f
Emmet Murray <ehmurray8@gmail.com>
2023-11-02 13:19:20 -0600

    Add Terms.

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,8 +4,9 @@
     <p class="italic text-xs text-center">
       203.906.3837 | carly@wildgracevideography.com | Denver, CO
     </p>
-    <NuxtLink class="text-xs mx-auto mt-4 w-fit block" to="/privacy-policy"
-      >Privacy Policy</NuxtLink
-    >
+    <div class="text-xs mx-auto mt-4 w-fit">
+      <NuxtLink class="mr-4" to="/privacy-policy">Privacy Policy</NuxtLink>
+      <NuxtLink to="/terms">Terms of Use</NuxtLink>
+    </div>
   </footer>
 </template>
