3bbc027d6141b6d192dd19cc70054a0cf77222f2
Emmet Murray <ehmurray8@gmail.com>
2023-11-02 12:12:58 -0600

    Add Privacy Policy.

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,5 +4,8 @@
     <p class="italic text-xs text-center">
       203.906.3837 | carly@wildgracevideography.com | Denver, CO
     </p>
+    <NuxtLink class="text-xs mx-auto mt-4 w-fit block" to="/privacy-policy"
+      >Privacy Policy</NuxtLink
+    >
   </footer>
 </template>
