30e4ac695be7213d3131eb16505438604d267806
Emmet Murray <ehmurray8@gmail.com>
2023-12-01 21:17:36 -0700

    Fix purchase success messaging.

--- pages/purchase-success.vue
+++ pages/purchase-success.vue
@@ -2,16 +2,16 @@
   <OgMeta :title="pageTitle" :description="description" />
   <SchemaOrgWebPage type="AboutPage" :name="pageTitle" />
   <section class="mx-auto w-fit text-center">
-    <h1 class="text-4xl mb-4">{{ heading }}</h1>
-    <h3 class="text-2xl text-center">Have Questions? Contact me here</h3>
-    <p class="mt-8">
-      Email me at
+    <h1 class="text-4xl mb-6">{{ heading }}</h1>
+    <!-- <h3 class="text-2xl text-center">Have Questions? Contact me here</h3> -->
+    <h3 class="text-lg text-center max-w-4xl">
+        If it doesn’t arrive in your email inbox within 15 minutes, please email me at,
       <a
-        class="underline text-website-green"
-        href="mailto:carly@wildgracevideography.com?subject=Purchase%20Questions"
+        class="underline text-website-green inline-block"
+        href="mailto:carly@wildgracevideography.com?subject=Purchase%20Help"
         >carly@wildgracevideography.com</a
-      >
-    </p>
+      >. Also, check spam to see if it went there!
+    </h3>
   </section>
 </template>
 
@@ -20,6 +20,7 @@ const pageTitle = "Wild Grace Videography | Thank You For Your Purchase!";
 const heading = "Thank You For Your Purchase!";
 const description =
   "Thank you for your purchase, please reach out to Wild Grace Videography if you have any questions.";
+
 definePageMeta({
   layout: "no-contact",
 });
