6a1f0bf276c97bb5ff1f2f747d39f5c41cb2937d
Emmet Murray <ehmurray8@gmail.com>
2024-03-19 02:23:33 +0000

    Add support for LogoSlider width

--- components/LogoSlider.vue
+++ components/LogoSlider.vue
@@ -5,7 +5,10 @@
         <!-- Heights and widths must match the logos-slide width defined below -->
         <div
             class="logos-slide inline-block h-16 whitespace-nowrap md:h-28"
-            :style="{ '--total-icons': logos.length }"
+            :style="{
+                '--total-icons': logos.length,
+                '--total-icon-width': totalIconWidth,
+            }"
         >
             <img
                 v-for="logo in logos"
@@ -14,6 +17,7 @@
                 :alt="logo.altText"
                 loading="lazy"
                 class="logo-aspect-ratio my-0 mr-20 inline-block max-h-full md:mr-40"
+                :style="{ '--icon-width': `${logo.width}px` }"
             />
             <img
                 v-for="logo in logos"
@@ -22,15 +26,24 @@
                 :alt="logo.altText"
                 loading="lazy"
                 class="logo-aspect-ratio my-0 mr-20 inline-block max-h-full md:mr-40"
+                :style="{ '--icon-width': `${logo.width}px` }"
             />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    defineProps<{
-        logos: { image: string; altText: string; companyName: string }[];
+    const props = defineProps<{
+        logos: {
+            image: string;
+            altText: string;
+            companyName: string;
+            width: number;
+        }[];
     }>();
+    const totalIconWidth = props.logos
+        .map((it) => it.width)
+        .reduce((total, current) => total + current, 0);
 </script>
 
 <style scoped>
@@ -73,8 +86,12 @@
     }
 
     .logos-slide {
-        width: calc(var(--total-icons) * (7rem + 10rem) * 2);
+        width: calc((var(--total-icons) * 10rem + var(--total-icon-width)) * 2);
         animation: calc(3s * var(--total-icons)) slide infinite linear;
+    }
+
+    .logos img {
+        width: var(--icon-width);
     }
 
     @media (max-width: 768px) {
@@ -83,8 +100,14 @@
             width: 100px;
         }
 
+        .logos img {
+            width: calc(var(--icon-width) * 0.75);
+        }
+
         .logos-slide {
-            width: calc(var(--total-icons) * (4rem + 5rem) * 2);
+            width: calc(
+                (var(--total-icons) * 5rem + var(--total-icon-width) * 0.75) * 2
+            );
         }
     }
 </style>
